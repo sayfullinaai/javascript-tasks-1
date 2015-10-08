f3cc92b4a976c19d8cb16be78125ee4b78ede0ae
sayfullinaai <sayfullinaai@gmail.com>
2015-10-08 14:55:44 +0500

    Update roman-time.js
    Добавила второй аргумент(система счисления) в patseInt.
    Использовала при сравнении текущего числа number  с 0 ===.
    Добавила обрамление {} тела оператора if.
    Изменила имя переменной n --> numberOfLength.
    Изменила название фукций isInputInteger и isInputValid на isValidInput и isValidInteger, соответсвенно.
    При вызове функции convertToRoman убрала +, так как в переменных hours и minutes уже лежат числа.

--- roman-time.js
+++ roman-time.js
@@ -5,34 +5,35 @@ var arabic = [1, 4, 5 , 9, 10, 40, 50];
 var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
 
 function convertToRoman(number){
-    if(number == 0)
+    if(number === 0){
         return '--';
-    var n = arabic.length-1;
+    }
+    var numberOfLength = arabic.length-1;
     var result = '';
     while(number > 0){
-        if(number >= arabic[n]){
-            result += roman[n];
-            number -= arabic[n];
+        if(number >= arabic[numberOfLength]){
+            result += roman[numberOfLength];
+            number -= arabic[numberOfLength];
         }
-        else n--;
+        else numberOfLength--;
     }
     return result;
 }
 
-function isInputInteger(minutes, hours){
-    return (hours == parseInt(hours)) && (minutes == parseInt(minutes));
+function isValidInput(minutes, hours){
+    return (hours == parseInt(hours, 10)) && (minutes == parseInt(minutes, 10));
 }
 
-function isInputValid(minutes, hours){
+function isValidInteger(minutes, hours){
     return !(hours < 0 || hours > 23 || minutes < 0 || minutes > 59);
 }
 
 var hours = process.argv[2];
 var minutes = process.argv[3];
 
-if (! isInputInteger(minutes, hours))
+if (! isValidInput(minutes, hours))
     console.log('You have entered an invalid number. Numbers must be an integer.');
-else if(! isInputValid(minutes, hours))
+else if(! isValidInteger(minutes, hours))
     console.log('Incorrect time.');
 else
-    console.log(convertToRoman(+hours)+':'+convertToRoman(+minutes));
+    console.log(convertToRoman(hours)+':'+convertToRoman(minutes));
